e90000415fb8a42533c00373101dd82a3ae4a28b
kafi-git <engr.kafi@gmail.com>
2022-09-09 19:38:04 +0600

    Add files via upload

--- detect.py
+++ detect.py
@@ -119,6 +119,7 @@ def run(
         with dt[1]:
             visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
             pred = model(im, augment=augment, visualize=visualize)
+            print(pred)
 
         # NMS
         with dt[2]:
